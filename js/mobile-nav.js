fd2f97d3fdf6edfd138d64a04e98bc7813cfdf50
POHAIDA <114564806+POHAIDA@users.noreply.github.com>
2023-03-28 09:26:43 +0300

    Add files via upload

---
+++ js/mobile-nav.js
@@ -0,0 +1,22 @@
+//mobile bav button
+
+const navBtn = document.querySelector('.mobile-nav-btn');
+const nav = document.querySelector('.mobile-nav');
+const menuIcon = document.querySelector('.nav-icon');
+const fade = document.querySelector('.mobile-nav-fade')
+
+
+navBtn.onclick = () => {
+    nav.classList.toggle('mobile-nav--open');
+    fade.classList.toggle('mobile-nav-fade--open');
+    menuIcon.classList.toggle('nav-icon--active');
+    document.body.classList.toggle('no-scroll');
+}
+
+fade.onclick = () => {
+    nav.classList.toggle('mobile-nav--open');
+    fade.classList.toggle('mobile-nav-fade--open');
+    menuIcon.classList.toggle('nav-icon--active');
+    document.body.classList.toggle('no-scroll');
+
+}
